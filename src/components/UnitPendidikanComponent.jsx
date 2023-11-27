3c2aa888998f692d761d64cdbde9fde08bb23681
herbisaptikopti <herbisaptikopti@gmail.com>
2023-11-27 08:57:53 -0800

    commit ke 7

---
+++ src/components/UnitPendidikanComponent.jsx
@@ -0,0 +1,36 @@
+import { Container, Row, Col, Card } from 'react-bootstrap'
+import yoga from '../assets/img/icon-yoga.png'
+
+const UnitPendidikanComponent = () => {
+    return (
+        <div className="Unitsekolah w-100 min-vh-100 d-flex align-items-center">
+                <Container>
+                    <Row className='sekolah-box d-flex align-items-center'>
+                        <Col>
+                            <h1 className='lg-mb-5 mb-3 fw-bold text-center'>UNIT PENDIDIKAN</h1>
+                        </Col>
+                    </Row>
+
+                    <Row className='mt-3 mb-5 justify-content-center g-2'>
+                        <Col className='mb-3 col-md-4 lg-mx-0 mx-5'>
+                        <Card className='kartu shadow'>
+                            <img src={yoga} />
+                            <Card.Body>
+                                <h5 className='fw-bold text-center'>SMP <br></br>ROUDLOTUT THULLAB<br></br>  BELITANG</h5>
+                            </Card.Body>
+                        </Card>
+                        </Col>
+                        <Col className='col-md-4 lg-mx-0 mx-5'>
+                        <Card className='kartu shadow'>
+                            <img src={yoga} />
+                            <Card.Body>
+                                <h5 className='fw-bold text-center'>SMK <br></br>ROUDLOTUT THULLAB<br></br>  BELITANG</h5>
+                            </Card.Body>
+                        </Card>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+    )
+}
+export default UnitPendidikanComponent
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,9 +2,8 @@ import { Container, Row, Col } from 'react-bootstrap'
 import HeroImage from '../assets/img/fotosmk.png'
 import Jokowi from '../assets/img/Jokowi.png'
 import kitab from '../assets/img/al-quran.png'
-import { Card } from 'react-bootstrap'
-import yoga from '../assets/img/icon-yoga.png'
 import maps from '../assets/img/maps.png'
+import UnitPendidikanComponent from '../components/UnitPendidikanComponent'
 
 const HomePage = () => {
     return (
@@ -54,34 +53,10 @@ const HomePage = () => {
                 </Container>
             </div>
             {/* ini section unit sekolah */}
-            <div className="Unitsekolah w-100 min-vh-100 d-flex align-items-center">
-                <Container>
-                    <Row className='sekolah-box d-flex align-items-center'>
-                        <Col>
-                            <h1 className='lg-mb-5 mb-3 fw-bold text-center'>UNIT PENDIDIKAN</h1>
-                        </Col>
-                    </Row>
-
-                    <Row className='mt-3 mb-5 justify-content-center g-2'>
-                        <Col className='mb-3 col-md-4 lg-mx-0 mx-5'>
-                        <Card className='kartu shadow'>
-                            <img src={yoga} />
-                            <Card.Body>
-                                <h5 className='fw-bold text-center'>SMP <br></br>ROUDLOTUT THULLAB<br></br>  BELITANG</h5>
-                            </Card.Body>
-                        </Card>
-                        </Col>
-                        <Col className='col-md-4 lg-mx-0 mx-5'>
-                        <Card className='kartu shadow'>
-                            <img src={yoga} />
-                            <Card.Body>
-                                <h5 className='fw-bold text-center'>SMK <br></br>ROUDLOTUT THULLAB<br></br>  BELITANG</h5>
-                            </Card.Body>
-                        </Card>
-                        </Col>
-                    </Row>
-                </Container>
+            <div className="unitsekolah w-100 min-vh-100">
+                <UnitPendidikanComponent />
             </div>
+
             <div className="santribaru">
                 <Container>
                     <Row className='text-center'>
