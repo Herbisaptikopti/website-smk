7ed972442b93d64d7b369b628dece47bacea2a96
herbisaptikopti <herbisaptikopti@gmail.com>
2023-11-28 21:27:55 -0800

    ubah hero image dan logo

--- src/components/NavComponent.jsx
+++ src/components/NavComponent.jsx
@@ -1,52 +1,63 @@
-import { useState, useEffect } from "react"
-import { Navbar, Container, Nav } from 'react-bootstrap'
-import { NavLink } from 'react-router-dom'
-import { navLink } from '../data/index'
-import logosmp from '../assets/img/logosmp.png'
+import { useState, useEffect } from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { navLink } from "../data/index";
+import logosmp from "../assets/img/logosmp.png";
+import logo from "/logo.svg";
 
 const NavComponent = () => {
-    // ini untuk mengatur ketika halaman di scrol navbar akan berganti warna
-    const[changeColor, setchangeColor] = useState(false);
+  // ini untuk mengatur ketika halaman di scrol navbar akan berganti warna
+  const [changeColor, setchangeColor] = useState(false);
 
-    const changeBackgroundColor = () => {
-        if (window.scrollY > 10 ) {
-            setchangeColor(true);
-        } else {
-            setchangeColor(false);
-        }
-    };
+  const changeBackgroundColor = () => {
+    if (window.scrollY > 10) {
+      setchangeColor(true);
+    } else {
+      setchangeColor(false);
+    }
+  };
 
-    useEffect(() => {
-        changeBackgroundColor();
+  useEffect(() => {
+    changeBackgroundColor();
 
-        window.addEventListener("scroll", changeBackgroundColor);
-    })
-    return (
-        <div>
-            <Navbar expand="lg" className={changeColor ? "color-active" : ""}>
-                <Container>
-                    <img src={logosmp}></img>
-                    <Navbar.Brand href="#home" className='smk fw-bold mx-3'>SMK / RTB</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav" className="hp navbar-light">
-                    <Nav className="mx-auto text-center">
-                        {navLink.map((link) => {
-                            return (
-                                <div className="nav-link" key={link.id}>
-                                    <NavLink to={link.path} className={({ isActive, isPending }) =>
-                                        isPending ? "pending" : isActive ? "active" : ""
-                                    }>{link.text}</NavLink>
-                                </div>
-                            )
-                        })}
-                    </Nav>
-                        <div className="text-center">
-                            <button className='btn btn-outline-warning rounded-1'>Login</button>
-                        </div>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </div>
-    )
-}
-export default NavComponent
+    window.addEventListener("scroll", changeBackgroundColor);
+  });
+  return (
+    <div>
+      <Navbar expand="lg" className={changeColor ? "color-active" : ""}>
+        <Container>
+          <img src={logo}></img>
+          <Navbar.Brand
+            href="#home"
+            className="smk fw-bold mx-3"
+          ></Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav" className="hp navbar-light">
+            <Nav className="mx-auto text-center">
+              {navLink.map((link) => {
+                return (
+                  <div className="nav-link" key={link.id}>
+                    <NavLink
+                      to={link.path}
+                      className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "active" : ""
+                      }
+                    >
+                      {link.text}
+                    </NavLink>
+                  </div>
+                );
+              })}
+            </Nav>
+            <div className="text-center">
+              <button className="btn btn-outline-warning rounded-1">
+                Login
+              </button>
+            </div>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+export default NavComponent;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,81 +1,108 @@
-import { Container, Row, Col } from 'react-bootstrap'
-import HeroImage from '../assets/img/fotosmk.png'
-import Jokowi from '../assets/img/Jokowi.png'
-import kitab from '../assets/img/al-quran.png'
-import maps from '../assets/img/maps.png'
-import UnitPendidikanComponent from '../components/UnitPendidikanComponent'
+import { Container, Row, Col } from "react-bootstrap";
+import hero from "../assets/img/hero.svg";
+import Jokowi from "../assets/img/Jokowi.png";
+import kitab from "../assets/img/al-quran.png";
+import maps from "../assets/img/maps.png";
+import UnitPendidikanComponent from "../components/UnitPendidikanComponent";
 
 const HomePage = () => {
-    return (
-        <div className="Homepage">
-            {/* header di sini untuk uuran warna background */}
-            <header className="w-100 min-vh-100 d-flex align-items-end ">
-                <Container>
-                    <Row className='header-box d-flex align-items-center'>
-                        <Col lg="6">
-                            <h1 className="mb-4 mt-5">بِسْمِ اللَّهِ الرحمن الرَّحِيمِ</h1>
-                            <h5>SELAMAT DATANG DI WEBSITE RESMI</h5>
-                            <h5>YAYASAN PONDOK PESANTREN PUJORAHAYU</h5>
-                            <h5>SMP DAN SMK</h5>
-                            <h5>ROUDLOTUT THULLAB BELITANG</h5>
-                            <button className="btn btn-warning btn-lg rounded-1 me-2 mb-xs-0 mb-2">Daftar</button>
-                            <button className="btn btn-outline-warning btn-lg rounded-1 mb-xs-0 mb-2">Informasi</button>
-                        </Col>
-                        <Col lg="6" className="mt-lg-5 pt-lg-0 pt-5 m-0 py-0">
-                            <img src={HeroImage} alt="hero-img" />
-                        </Col>
-                    </Row>
-                </Container>
-            </header>
-            {/* ini section sambutan kepala sekolah */}
-            <div className="Sambutan w-100 min-vh-100 d-flex align-items-center">
-                <Container>
-                    <Row className='sambutan-box d-flex align-items-start'>
-                        <Col lg="4">
-                            <img src={Jokowi} alt="kepala-sekolah-img" />
-                        </Col>
-                        <Col lg="8">
-                            <h2 className='fw-bold mb-5 mt-3'>PRAKATA KEPALA SEKOLAH</h2>
-                            <p>Era globalisasi dengan segala implikasinya menjadi salah satu pemicu cepatnya perubahan yang terjadi pada berbagai aspek kehidupan masyarakat, dan bila tidak ada upaya sungguh-sungguh untuk mengantisipasinya maka hal tersebut akan menjadi maslah yang sangat serius. Dalam hal ini dunia pendidikan mempunyai tanggung jawab yang besar, terutama dalam menyiapkan sumber daya manusia yang tangguh sehingga mampu hidup selaras didalam perubahan itu sendiri. Pendidikan merupakan investasi jangka panjang yang hasilnya tidak dapat dilihat dan dirasakan secara instan, sehingga sekolah sebagai ujung tombak dilapangan harus memiliki arah pengembangan jangka panjang dengan tahapan pencapaiannya yang jelas dan tetap mengakomodir tuntutan permasalahan faktual kekinian yang ada di masyarakat.</p>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-            {/* ini section kitab */}
-            <div className="Kitab w-100 lg-min-vh-100 min-vh-50 d-flex align-items-center">
-                <Container>
-                    <Row className='kitab-box d-flex align-items-center text-center'>
-                        <Col>
-                            <h1 className='fw-bold mb-5'>Sayyidina Ali pernah berkata <br></br>bahwa ilmu pengetahuan lebih baik daripada harta.</h1>
-                            <img src={kitab} alt="kitab-img" />
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-            {/* ini section unit sekolah */}
-            <div className="unitsekolah w-100 min-vh-100">
-                <UnitPendidikanComponent />
-            </div>
+  return (
+    <div className="Homepage">
+      {/* header di sini untuk uuran warna background */}
+      <header className="w-100 min-vh-100 d-flex align-items-center ">
+        <Container>
+          <Row className="header-box d-flex flex-col align-items-center text-center">
+            <Col lg="6" className="mt-lg-5 pt-lg-0 pt-2 m-0 py-0">
+              <img src={hero} alt="hero-img" />
+            </Col>
 
-            <div className="santribaru">
-                <Container>
-                    <Row className='text-center'>
-                        <Col>
-                            <h1 className='mt-5 text-center'>PENDAFTARAN SANTRI BARU <br></br>TAHUN AJARAN 2023/2024</h1>
-                            <button className='mt-5 px-5 btn btn-warning rounded-1'>Daftar</button>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-            <div className="maps w-100 lg-min-vh-100 min-vh-50">
-                <Container>
-                    <Row className='maps-box'>
-                        <h1 className='fw-bold mb-5 text-center'>GOOGLE MAP</h1>
-                        <img src={maps} alt="maps-img" />
-                    </Row>
-                </Container>
-            </div>
-        </div>
-    )
-}
-export default HomePage
+            <Col lg="6" className="">
+              <h1 className="mb-4 mt-0">بِسْمِ اللَّهِ الرحمن الرَّحِيمِ</h1>
+              <hr className="mt-0" />
+              <h5>SELAMAT DATANG DI WEBSITE RESMI</h5>
+              <h5>YAYASAN PONDOK PESANTREN PUJORAHAYU</h5>
+              <h5>SMP DAN SMK</h5>
+              <h5>ROUDLOTUT THULLAB BELITANG</h5>
+              <button className="btn btn-warning btn-lg rounded-1 me-2 mb-xs-0 mb-2 px-5">
+                DAFTAR
+              </button>
+              <button className="btn btn-outline-warning btn-lg rounded-1 mb-xs-0 mb-2 px-4">
+                INFORMASI
+              </button>
+            </Col>
+          </Row>
+        </Container>
+      </header>
+      {/* ini section sambutan kepala sekolah */}
+      <div className="Sambutan w-100 min-vh-100 d-flex align-items-center">
+        <Container>
+          <Row className="sambutan-box d-flex align-items-start">
+            <Col lg="4">
+              <img src={Jokowi} alt="kepala-sekolah-img" />
+            </Col>
+            <Col lg="8">
+              <h2 className="fw-bold mb-5 mt-3">PRAKATA KEPALA SEKOLAH</h2>
+              <p>
+                Era globalisasi dengan segala implikasinya menjadi salah satu
+                pemicu cepatnya perubahan yang terjadi pada berbagai aspek
+                kehidupan masyarakat, dan bila tidak ada upaya sungguh-sungguh
+                untuk mengantisipasinya maka hal tersebut akan menjadi maslah
+                yang sangat serius. Dalam hal ini dunia pendidikan mempunyai
+                tanggung jawab yang besar, terutama dalam menyiapkan sumber daya
+                manusia yang tangguh sehingga mampu hidup selaras didalam
+                perubahan itu sendiri. Pendidikan merupakan investasi jangka
+                panjang yang hasilnya tidak dapat dilihat dan dirasakan secara
+                instan, sehingga sekolah sebagai ujung tombak dilapangan harus
+                memiliki arah pengembangan jangka panjang dengan tahapan
+                pencapaiannya yang jelas dan tetap mengakomodir tuntutan
+                permasalahan faktual kekinian yang ada di masyarakat.
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      {/* ini section kitab */}
+      <div className="Kitab w-100 lg-min-vh-100 min-vh-50 d-flex align-items-center">
+        <Container>
+          <Row className="kitab-box d-flex align-items-center text-center">
+            <Col>
+              <h1 className="fw-bold mb-5">
+                Sayyidina Ali pernah berkata <br></br>bahwa ilmu pengetahuan
+                lebih baik daripada harta.
+              </h1>
+              <img src={kitab} alt="kitab-img" />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      {/* ini section unit sekolah */}
+      <div className="unitsekolah w-100 min-vh-100">
+        <UnitPendidikanComponent />
+      </div>
+
+      <div className="santribaru">
+        <Container>
+          <Row className="text-center">
+            <Col>
+              <h1 className="mt-5 text-center">
+                PENDAFTARAN SANTRI BARU <br></br>TAHUN AJARAN 2023/2024
+              </h1>
+              <button className="mt-5 px-5 btn btn-warning rounded-1">
+                Daftar
+              </button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div className="maps w-100 lg-min-vh-100 min-vh-50">
+        <Container>
+          <Row className="maps-box">
+            <h1 className="fw-bold mb-5 text-center">GOOGLE MAP</h1>
+            <img src={maps} alt="maps-img" />
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
+export default HomePage;
